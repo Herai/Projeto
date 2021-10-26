80541ede832a0e2675628e12376c270d42824f7d
Herai <88412624+Herai@users.noreply.github.com>
2021-10-26 11:46:28 -0300

    Update Gerenciameto_De_Gastos.c

--- Gerenciameto_De_Gastos.c
+++ Gerenciameto_De_Gastos.c
@@ -1,33 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-float salario, Gastos;
+float salario, Gastos[];
+int Decisao, i;
+
+
+//Talvez Usar 2 Vetores,um para o gasto e outro para a Descrição
+//E Posteriormente Usar BubbleSort para organizalos e Mostra-los Organizados no Final do Programa 
+
 
 float Entrada(){
-
-    printf("Digite sua renda salarial: ");
+    printf("Ola,Por Favor Digite sua renda salarial: ");
     scanf("%f", &salario);
 
-    for(int i=0; i<1000; i++){
+    for(i=0; i<1000; i++){
 
         printf("Digite seu gasto: ");
+        scanf("%f", &Gastos[i]);   
+
+        printf("Deseja inserir outro gasto? \n1- Sim \n2-Nao");
+        scanf("%f", &Decisao);
+
+        if (Decisao == 1)
+        {
+            
+        }
         
-
-
+    }
+    for(i=0; i < 200; i++)
+    {
+      printf("Seus gastos foram %f", &Gastos[i]);  
     }
 }
 
-
-
-
-
-
-
-
-
-
 int main(void){
 
-
+Entrada();
 
 }
