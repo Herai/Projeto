e266b24adf4614649c91609cfe4445b6b646dad7
Herai <herai798@gmail.com>
2021-10-25 21:25:17 -0300

    Create Gerenciameto_De_Gastos.c

---
+++ Gerenciameto_De_Gastos.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+float salario, Gastos;
+
+float Entrada(){
+
+    printf("Digite sua renda salarial: ");
+    scanf("%f", &salario);
+
+    for(int i=0; i<1000; i++){
+
+        printf("Digite seu gasto: ");
+        
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+int main(void){
+
+
+
+}
