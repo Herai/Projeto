6e1beb624e6c103bc74a127df69d420fba4c607e
Herai <88412624+Herai@users.noreply.github.com>
2021-10-29 11:44:17 -0300

    Update Gerenciameto_De_Gastos.c

--- Gerenciameto_De_Gastos.c
+++ Gerenciameto_De_Gastos.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-float salario, Gastos[];
-int Decisao, i;
+float salario, Gastos[], SalarioFinal;
+int Decisao, i ,j;
 
 
 //Talvez Usar 2 Vetores,um para o gasto e outro para a Descrição
@@ -12,29 +12,46 @@ int Decisao, i;
 float Entrada(){
     printf("Ola,Por Favor Digite sua renda salarial: ");
     scanf("%f", &salario);
+}
 
-    for(i=0; i<1000; i++){
+float Processo(){
+    SalarioFinal = salario;
+
+    for(i=0; i<5; i++){
 
         printf("Digite seu gasto: ");
-        scanf("%f", &Gastos[i]);   
-
-        printf("Deseja inserir outro gasto? \n1- Sim \n2-Nao");
-        scanf("%f", &Decisao);
-
-        if (Decisao == 1)
-        {
-            
+         scanf("%f", &Gastos[i]);
+         break;          
+      }
+        for(i=0; i<5; i++){
+          printf("Seus gastos foram : \n%f", Gastos[i]);
         }
         
-    }
-    for(i=0; i < 200; i++)
-    {
-      printf("Seus gastos foram %f", &Gastos[i]);  
-    }
+        /*if(SalarioFinal <= 500){
+
+          for(j=0; j <1000; j++){
+
+          printf("Seu gastos foram : %f", &Gastos[j]);
+
+          break;
+
+          }
+        }*/
+        //printf("Deseja inserir outro gasto? \n1- Sim \n2-Nao");
+        //scanf("%f", &Decisao);
 }
+
+
+float Saida(){
+for(i=0; i < 200; i++){
+  printf("Seus gastos foram %f", &Gastos[i]);  
+  }
+}
+
 
 int main(void){
 
 Entrada();
+Processo();
 
 }
